e64380bd23a325caa40d564fafa3cb6dff17d474
M1ghel-Angello <mighelangello@gmail.com>
2025-08-20 20:05:06 -0300

    IRPF Funcionando

--- ConceitosBásicos/Program.cs
+++ ConceitosBásicos/Program.cs
@@ -1,7 +1,32 @@
-﻿byte idade;
-string nome;
-Console.Write("Digite seu nome:");
-nome = Console.ReadLine();
-Console.Write("Digite sua idade:");
-idade = Convert.ToByte(Console.ReadLine());
-Console.WriteLine("Olá " + nome + ". A idade informada foi {0)", idade);
+﻿//using System.ComponentModel.DataAnnotations;
+
+//byte idade;
+//string nome;
+//Console.Write("Digite seu nome:");
+//nome = Console.ReadLine();
+//Console.Write("Digite sua idade:");
+//idade = Convert.ToByte(Console.ReadLine());
+//Console.WriteLine("Olá " + nome + ". A idade informada foi {0)", idade);
+//decimal salario = 50000.99m;
+//idade *= 2; // idade = idade * 2
+//Console.WriteLine($"A nova idade é {idade++}");//40
+//Console.WriteLine($"A nova idade é {++idade}"); //42
+
+//Altere o programa para que ele solicite ao usuário informar o seu salário e ao final informe o desconto de IRPF de acordo com o salário informado. Utilize a tabela oficial de descontos de IRPF do Brasil
+
+
+decimal salario, irpfDevido;
+Console.Write("Informe o salário: ");
+salario = decimal.Parse(Console.ReadLine());
+
+if (salario <= 2428.8m)
+    irpfDevido = 0;
+else if (salario <= 2428.81m && salario <= 2826.65m)
+    irpfDevido = salario * 0.075m - 182.16m;
+else if (salario <= 3751.05m)
+    irpfDevido = salario * 0.15m - 394.16m;
+else if (salario <= 4664.68m)
+    irpfDevido = salario * 0.225m - 675.49m;
+else
+    irpfDevido = salario * 0.275m - 908.73m;
+Console.WriteLine($"Para o salário {salario: C2} o IRPF a ser pago é: {irpfDevido: C2}");
